887853e70fc530fae90f060df7017752eb826e1b
jeet-A10 <jeetyadav414141@gmail.com>
2026-04-17 17:07:30 +0000

    variables,datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+let accountId=1554
+const accountCity="ujjain"
+// const me jo bhi value store hogi usko hum log change nahi kr skte baad me kisi or value ke sath
+
+var accountPassword=525564
+accountSTate="madhya pradesh"
+
+accountID=5456589
+accountSTate="maharashtra"
+// const ki value ko change nahi kr skte
+
+/*
+let and var are the same type of keyword in which we can store information in the memory which in changeabl in dono me apn change ya dusri value assign kr skte h baad me bhi
+*/ 
+console.log(accountId);
+console.table([accountCity,accountId,accountPassword,accountSTate])
+
